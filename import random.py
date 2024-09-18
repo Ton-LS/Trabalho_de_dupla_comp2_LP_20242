8fc92531b6f1d5cab78cf5fcf7fb7e583ede7435
blablablanco <blanco8446@gmail.com>
2024-09-18 13:06:26 -0300

    charmeleon quase pronto ai

--- import random.py
+++ import random.py
@@ -1,7 +1,9 @@
 import random
 class Pokemon:
-    def __init__(self, bichinho, elemento,  alvo):
+    def __init__(self, bichinho, vida, evolucao ,  elemento,  alvo):
         self.bichinho = bichinho
+        self.vida = vida
+        self.evolucao = evolucao
         self.elemento = elemento
         self.alvo = alvo
     def ataquebasico(self,alvo): 
@@ -11,14 +13,46 @@ class Pokemon:
         #ataqueerrado 1-4
         if 8<=x<=10:
             print('O ataque foi crítico!')
-            alvovida = alvo - 5
+            alvo.vida = alvo - 5
         elif 5<=x<=7:
             print('O ataque foi normal!')
-            alvovida = alvo - 3
+            alvo.vida = alvo - 3
         else: 
             print('O ataque foi fraco!')
-            alvovida = alvo -1
+            alvo.vida = alvo -1
 class Charmander(Pokemon):
     def __init__(self):
-        Pokemon.__init__(self, Charmander, 'Fogo', )
+        Pokemon.__init__(self, bichinho = 'Charmander', vida = 100 , elemento ='Fogo', alvo )
     def flameburst(self):
+        x = random.choice[1,2,3,4,5,6,7,8,9,10]
+        #ataquecritico 8-10
+        #ataquenormal 5-7
+        #ataqueerrado 1-4
+        if 8<=x<=10:
+            print(f'O {self.bichinho} utilizou o Flame Burst! Acerto em cheio!')
+            alvo.vida = alvo - 15
+        elif 5<=x<=7:
+            print('O {self.bichinho} utilizou o Flame Burst! Acerto normal!')
+            alvo.vida = alvo - 9
+        else: 
+            print('O {self.bichinho} utilizou o Flame Burst! O ataque foi fraco!')
+            alvo.vida = alvo -3
+    def dragonrage(self):
+        x = random.choice[1,2,3,4,5,6,7,8,9,10]
+        #ataquecritico 8-10
+        #ataquenormal 5-7
+        #ataqueerrado 1-4
+        if 8<=x<=10:
+            print(f'O {self.bichinho} utilizou o Dragon Rage! Acerto em cheio!')
+            alvo.vida = alvo - 20
+        elif 5<=x<=7:
+            print('O {self.bichinho} utilizou o Dragon Rage! Acerto normal!')
+            alvo.vida = alvo - 12
+        else: 
+            print('O {self.bichinho} utilizou o Dragon Rage! O ataque foi fraco!')
+            alvo.vida = alvo - 4
+class Charmeleon(Pokemon, Charmander):
+    def __init__(self):
+        Pokemon.__init__(self, bichinho = 'Charmeleon', vida = 200 , elemento ='Fogo', alvo )
+    
+    
