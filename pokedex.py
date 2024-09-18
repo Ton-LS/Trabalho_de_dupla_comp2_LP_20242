24cf2daf2444af82dfdf54f3ba65ef319c01a54a
Ton-LS <78545780+Ton-LS@users.noreply.github.com>
2024-09-18 11:43:36 -0300

    Update pokedex.py
    apaguei os seguintes atributos de todos as classes   super_vulnerável_contra = []
        pouco_vulnerável_contra = []
        invulnerabilidade = []

--- pokedex.py
+++ pokedex.py
@@ -17,59 +17,88 @@ class normal(pokemon):
     super_efetivo_contra = []
     pouco_efetivo_contra = ["pedra","aço" ]
     inefetivo_contra =["fantasma"]
-    super_vulnerável_contra = ["lutador"]
-    pouco_vulnerável_contra = []
-    invulnerabilidade = ["fantasma"]
 
 class lutador(pokemon):
     super_efetivo_contra = ["normal","pedra","aço","gelo","sombrio"]
     pouco_efetivo_contra = ["voador","venenoso", "inseto", "psíquico", "fada" ]
     inefetivo_contra =["fantasma"]
-    super_vulnerável_contra = ["voador", "psíquico", "fada"]
-    pouco_vulnerável_contra = ["pedra", "inseto", "sombrio"]
-    invulnerabilidade = []
 
-class voador:
+class voador(pokemon):
     super_efetivo_contra = ["lutador", "inseto", "planta"]
     pouco_efetivo_contra = ["pedra", "aço", "elétrico" ]
     inefetivo_contra =[]
-    super_vulnerável_contra = ["pedra", "elétrico", "gelo"]
-    pouco_vulnerável_contra = ["lutador", "inseto", "planta"]
-    invulnerabilidade = ["terrestre"]
 
-class venenoso:
+class venenoso(pokemon):
     super_efetivo_contra = ["planta", "fada"]
     pouco_efetivo_contra = ["venenoso","terrestre", "pedra", "fantasma" ]
     inefetivo_contra =["aço"]
-    super_vulnerável_contra = ["terrestre", "psíquico"]
-    pouco_vulnerável_contra = ["lutador", "venenoso", "inseto", "planta", "fada"]
-    invulnerabilidade = []
-class terrestre:
+
+class terrestre(pokemon):
+    super_efetivo_contra = ["venenoso", "pedra", "aço", "fogo"]
+    pouco_efetivo_contra = ["inseto","planta"]
+    inefetivo_contra =["voador"]
 
 class pedra:
+    super_efetivo_contra = ["voador", "inseto", "fogo", "gelo"]
+    pouco_efetivo_contra = ["lutador", "terrestre", "aço"]
+    inefetivo_contra =[]
 
 class inseto:
+    super_efetivo_contra = ["planta", "psíquico", "sombrio"]
+    pouco_efetivo_contra = ["lutador", "voador", "venenoso", "fantasma", "aço", "fogo", "fada"]
+    inefetivo_contra =[]
 
 class fantasma:
+    super_efetivo_contra = ["fantasma", "psíquico"]
+    pouco_efetivo_contra = ["sombrio"]
+    inefetivo_contra =["normal"]
 
 class aço:
-
+    super_efetivo_contra = ["pedra", "gelo", " fada"]
+    pouco_efetivo_contra = ["aço", "fogo", "água", "elétrico"]
+    inefetivo_contra =[]
+ 
 class fogo:
+    super_efetivo_contra = ["inseto", "aço", "planta", "gelo"]
+    pouco_efetivo_contra = ["pedra", "fogo", "água", "dragão"]
+    inefetivo_contra =[]
 
 class água:
-
+    super_efetivo_contra = ["terrestre","pedra", "fogo"]
+    pouco_efetivo_contra = ["água", "planta", "dragão"]
+    inefetivo_contra =[]
+  
 class planta:
+    super_efetivo_contra = ["terrestre", "pedra", "água"]
+    pouco_efetivo_contra = ["voador", "venenoso", "inseto", "aço", "fogo", "planta", "dragão"]
+    inefetivo_contra =[]
 
 class elétrico:
+    super_efetivo_contra = ["voador", "água"]
+    pouco_efetivo_contra = ["planta", "elétrico", "dragão"]
+    inefetivo_contra =["terrestre"]
 
 class psíquico:
+    super_efetivo_contra = ["lutador", "venenoso"]
+    pouco_efetivo_contra = ["aço", "psíquico"]
+    inefetivo_contra =["sombrio"]
 
 class gelo:
+    super_efetivo_contra = ["voador", "terrestre", "planta", "dragão"]
+    pouco_efetivo_contra = ["aço", "fogo", "água"]
 
 class dragão:
+    super_efetivo_contra = ["dragão"]
+    pouco_efetivo_contra = ["aço"]
+    inefetivo_contra =["fada"]
 
 class sombrio:
+    super_efetivo_contra = ["aço", "psíquico"]
+    pouco_efetivo_contra = ["lutador", "sombrio", "fada"]
 
-class fada
+class fada:
+    super_efetivo_contra = ["lutador", "dragão", "sombrio"]
+    pouco_efetivo_contra = ["venenoso", "aço", "fogo"]
+    inefetivo_contra =[]
 
     
